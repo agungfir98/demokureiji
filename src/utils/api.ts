153d899a99e558a6902c23b96b9bde5ced53174f
agungfir98 <agungfir98@gmail.com>
2023-08-16 09:36:39 +0700

    feat: create new api function with headers pre attached

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,8 +1,28 @@
-import axios from 'axios'
+import axios, { AxiosResponse } from 'axios'
 
-const { URI } = process.env
+const URI = process.env.NEXT_PUBLIC_URI
 
 export const api = axios.create({
-  baseURL: 'http://localhost:3000',
+  baseURL: URI,
   withCredentials: true,
 })
+
+const headers = (token?: string) => ({
+  headers: {
+    'auth-token': token ? `Bearer ${token}` : '',
+  },
+})
+
+export const apiGet = async <T>(
+  path: string,
+  accessToken?: string,
+): Promise<AxiosResponse<{ result: T }>> => {
+  return api.get(path, headers(accessToken))
+}
+
+export const apiPost = async <T>(
+  path: string,
+  accessToken?: string,
+): Promise<AxiosResponse<T>> => {
+  return api.post(path, headers(accessToken))
+}
