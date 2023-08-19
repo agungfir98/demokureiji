7ba70c956bc787dc4c535f7c789ac4bb5b685949
agungfir98 <agungfir98@gmail.com>
2023-08-19 11:51:20 +0700

    feat: add base size and success variant

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -2,7 +2,7 @@ import clsx from 'clsx'
 import { cva, VariantProps } from 'class-variance-authority'
 import { LoadingOutlined } from '@ant-design/icons'
 
-const ButtonVariance = cva('font-semibold flex gap-2 items-center', {
+const ButtonVariance = cva('font-semibold flex gap-2 items-center h-fit', {
   variants: {
     shape: {
       circle: 'rounded-full',
@@ -11,6 +11,7 @@ const ButtonVariance = cva('font-semibold flex gap-2 items-center', {
     },
     size: {
       sm: 'px-3 py-1 text-sm',
+      base: 'px-3 py-2 text-base',
       md: 'px-3 py-2 text-md',
       xl: 'px-5 py-3 text-xl',
     },
@@ -20,6 +21,7 @@ const ButtonVariance = cva('font-semibold flex gap-2 items-center', {
       primary: 'bg-blue-500 hover:bg-blue-400 text-white',
       warning: 'bg-yellow-500 hover:bg-yellow-400 text-black',
       danger: 'bg-red-500 hover:bg-red-400 text-white',
+      success: 'bg-green-500 text-white hover:bg-green-400 ',
       link: 'bg-transparent w-fit h-fit',
     },
   },
