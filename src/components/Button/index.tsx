335df800e61442a7c0dcf5bb6892d8bb235b99c0
agungfir98 <agungfir98@gmail.com>
2023-08-22 15:10:07 +0700

    chore: add text center

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -2,35 +2,38 @@ import clsx from 'clsx'
 import { cva, VariantProps } from 'class-variance-authority'
 import { LoadingOutlined } from '@ant-design/icons'
 
-const ButtonVariance = cva('font-semibold flex gap-2 items-center h-fit', {
-  variants: {
-    shape: {
-      circle: 'rounded-full',
-      round: 'rounded-3xl',
-      default: 'rounded-none',
+const ButtonVariance = cva(
+  'font-semibold flex gap-2 items-center h-fit justify-center',
+  {
+    variants: {
+      shape: {
+        circle: 'rounded-full',
+        round: 'rounded-3xl',
+        default: 'rounded-none',
+      },
+      size: {
+        sm: 'px-3 py-1 text-sm',
+        base: 'px-3 py-2 text-base',
+        md: 'px-3 py-2 text-md',
+        xl: 'px-5 py-3 text-xl',
+      },
+      variant: {
+        default:
+          'bg-white outline outline-2 outline-slate-300 text-slate-400 hover:outline-blue-400 hover:text-blue-400',
+        primary: 'bg-blue-500 hover:bg-blue-400 text-white',
+        warning: 'bg-yellow-500 hover:bg-yellow-400 text-black',
+        danger: 'bg-red-500 hover:bg-red-400 text-white',
+        success: 'bg-green-500 text-white hover:bg-green-400 ',
+        link: 'bg-transparent w-fit h-fit',
+      },
     },
-    size: {
-      sm: 'px-3 py-1 text-sm',
-      base: 'px-3 py-2 text-base',
-      md: 'px-3 py-2 text-md',
-      xl: 'px-5 py-3 text-xl',
+    defaultVariants: {
+      shape: 'default',
+      variant: 'default',
     },
-    variant: {
-      default:
-        'bg-white outline outline-2 outline-slate-300 text-slate-400 hover:outline-blue-400 hover:text-blue-400',
-      primary: 'bg-blue-500 hover:bg-blue-400 text-white',
-      warning: 'bg-yellow-500 hover:bg-yellow-400 text-black',
-      danger: 'bg-red-500 hover:bg-red-400 text-white',
-      success: 'bg-green-500 text-white hover:bg-green-400 ',
-      link: 'bg-transparent w-fit h-fit',
-    },
+    compoundVariants: [],
   },
-  defaultVariants: {
-    shape: 'default',
-    variant: 'default',
-  },
-  compoundVariants: [],
-})
+)
 
 interface CustomButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement>,
