232277215a645bd3961cc438cdd88866f685de13
agungfir98 <agungfir98@gmail.com>
2023-08-28 12:27:17 +0700

    refactor: candidates types

--- src/types/vote-event-type.ts
+++ src/types/vote-event-type.ts
@@ -1,18 +1,24 @@
 import { OrganizationType } from './organization-type'
 
+export interface CandidateType {
+  _id: string
+  calonKetua: string
+  calonWakil: string
+  description: string
+  image: { url: string }
+}
+
+interface Candidates {
+  numOfVotes: number
+}
+type Candidate = Candidates & CandidateType
+
 export interface VoteEventType {
   _id: string
   voteTitle: string
   isActive: boolean
   status: 'inactive' | 'active' | 'finished'
-  candidates: {
-    calonKetua: string
-    wakitKetua: string
-    description: string
-    numOfVotes?: number
-    _id?: string
-    image: { url: string }
-  }[]
+  candidates: Candidate[]
   holder: OrganizationType
   registeredVoters: {
     voter: { _id: string; name: string }
