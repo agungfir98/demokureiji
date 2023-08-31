0a23a585e88f619af953816df0503c69e3548864
agungfir98 <agungfir98@gmail.com>
2023-08-31 14:18:46 +0700

    chore: voteparticipation infer type

--- src/types/user-type.ts
+++ src/types/user-type.ts
@@ -1,9 +1,10 @@
 import { OrganizationType } from './organization-type'
+import { VoteEventType } from './vote-event-type'
 
 export interface UserType {
   _id: string
   name: string
   email: string
   organization: OrganizationType[]
-  voteParticipation: []
+  voteParticipation: VoteEventType[]
 }
